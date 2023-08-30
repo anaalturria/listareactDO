146d1748990ec0e65e10d777a057af7420af52c2
anaalturria <anavxsss@gmail.com>
2023-08-30 16:37:16 -0300

    Bloco de Notas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 
+
 function App() {
 
   const [ listaTarefas, setListaTarefas ] = useState( [] );
@@ -23,6 +24,7 @@ function App() {
       listaTarefas[index].status = !status;
       setListaTarefas( [...listaTarefas] );
   }
+  
 
   useEffect( () => {
     setTarefa( { id: "" , texto: "", status: ""});
@@ -30,21 +32,27 @@ function App() {
 
   return (
     <>
+
     <header>
       <h1>React DO</h1>
     </header>
-      <div>
-          <input type="text" name="tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( { id: Math.random(), texto: e.target.value, status: false} ) }></input>
-          <button onClick={addTarefa}>Adicionar</button>
-          
-      </div>
-      <div>
+    <div className="bloconotas">
+      <div className="lista">
           <ul>
             {listaTarefas.map( (item, index ) => (
-              <li key={index}>{item.texto} <button onClick={ () => statusTarefa(item.id, item.status) }>{item.status ? 'Concluida' : 'Não concluida'}</button> <button onClick={ () => excluirTarefa(item.id) }>Excluir</button></li>
+              <li className={item.status ? 'itemAtivo' : 'itemInativo'} key={index}>{item.texto} <button onClick={ () => statusTarefa(item.id, item.status) }>{item.status ? <i class="fa-solid fa-xmark"></i> : <i class="fa-solid fa-check"></i>  }</button> <button onClick={ () => excluirTarefa(item.id) }><i class="fa-solid fa-trash"></i></button></li>
             ))}
           </ul>
       </div>
+      <div className="addtarefa">
+          <input type="text" name="tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( { id: Math.random(), texto: e.target.value, status: false} ) }></input>
+          <button onClick={addTarefa}>Adicionar</button>
+      </div>
+      <div className="diario">
+        <h1>Como você esta se sentindo hoje?</h1>
+        <input type="text"></input>
+      </div>
+    </div>
     </>
   );
 }
